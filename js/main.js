24205906ae1dbe7dc658b01a9d62c21d56a5bd32
QQOld <artem.bataev2016@yandex.ru>
2020-10-07 17:16:27 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -20,4 +20,7 @@ var reviewsSlider = new Swiper('.reviews-slider', {
   },
 })
 
-$('.newsletter').paroller();
+var menuButton = document.querySelector(".menu-button")
+menuButton.addEventListener('click', function () {
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+})
